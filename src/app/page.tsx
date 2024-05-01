23d6aeeecc233012864a50f02d43a4467867b8fb
iglooe <day.derek7@protonmail.com>
2024-04-30 23:09:47 -0500

    use next image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,22 @@
 // disable caching of older images, and force new images to the ui ASAP.
 export const dynamic = "force-dynamic";
 
+import Image from "next/image";
+
 import { GetImages } from "~/server/queries";
 
 async function Images() {
   const images = await GetImages();
   return (
-    <div className="flex flex-wrap gap-4">
+    <div className="flex flex-wrap justify-center gap-4">
       {images.map((image) => (
         <div key={image.id} className="flex w-48 flex-col">
-          <img src={image.url} />
+          <Image
+            alt="uploaded image"
+            width={512}
+            height={320}
+            src={image.url}
+          />
           <div>{image.name}</div>
         </div>
       ))}
